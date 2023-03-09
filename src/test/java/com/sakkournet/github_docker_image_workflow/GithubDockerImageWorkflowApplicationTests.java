b98739e9dd1a40c969e17a68e89c95437b33ea84
ayoub sakkour <ayoub.sakkournet@example.com>
2023-03-09 08:59:28 +0100

    code

---
+++ src/test/java/com/sakkournet/github_docker_image_workflow/GithubDockerImageWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakkournet.github_docker_image_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDockerImageWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
