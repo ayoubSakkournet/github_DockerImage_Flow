5970772a565148942626425f1a2d997fece372d7
ayoub sakkour <ayoub.sakkournet@example.com>
2023-03-15 15:44:14 +0100

    changed2

--- src/main/java/com/sakkournet/github_docker_image_workflow/GithubDockerImageWorkflowApplication.java
+++ src/main/java/com/sakkournet/github_docker_image_workflow/GithubDockerImageWorkflowApplication.java
@@ -11,7 +11,7 @@ public class GithubDockerImageWorkflowApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to the Docker image ";
+		return "Welcome to the Docker image CI/CD ";
 	}
 
 
